5375cfad0b1eca7028c408d3eb000074eefb51a2
Silas Martins <silas.martins2041@gmail.com>
2023-04-06 07:36:11 -0300

    Resolve tests

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,7 +15,6 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
     if(!newTaskTitle) return;
 
     const newTask = {
@@ -29,12 +28,10 @@ export function TaskList() {
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
     setTasks((tasks.map(task => task.id === id ? {...task, isComplete : !task.isComplete}: task)))
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
     setTasks(tasks.filter(task => task.id !== id))
   }
 
